01e517c504ba055ca5ca19eeeba541e46572bc9d
Ноорузбек <nooruz@outlook.com>
2022-01-02 15:23:44 +0600

    02.01.2022

---
+++ RetailTradeServer/Converters/StringIsNullOrEmptyBooleanConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RetailTradeServer.Converters
+{
+    public class StringIsNullOrEmptyBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = string.IsNullOrEmpty(value.ToString());
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
