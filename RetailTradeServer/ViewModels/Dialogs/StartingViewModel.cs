e793995d7d98217415f7bf36020c9e8c4f2a3458
Ноорузбек <nooruz@outlook.com>
2022-01-10 10:27:49 +0600

    10.01.2022

--- RetailTradeServer/ViewModels/Dialogs/StartingViewModel.cs
+++ RetailTradeServer/ViewModels/Dialogs/StartingViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using RetailTradeServer.ViewModels.Dialogs.Base;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace SalePageServer.ViewModels.Dialogs
 {
     public class StartingViewModel : BaseDialogViewModel
     {
+        #region Public Properties
 
+        public string ApplicationName => $"{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductName} {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion}";
+
+        #endregion
     }
 }
