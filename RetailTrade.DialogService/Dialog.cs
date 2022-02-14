a94cff1f16f8c63cf0cd8a459711b14fb5b434b4
Ноорузбек <nooruz@outlook.com>
2022-02-14 07:59:02 +0600

    14.02.2022

---
+++ RetailTrade.DialogService/Dialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RetailTrade.DialogService
+{
+    public class Dialog : Control
+    {
+        static Dialog()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Dialog), new FrameworkPropertyMetadata(typeof(Dialog)));
+        }
+    }
+}
