829872630bc50a0c9421a08eb9e2aa56a07b57d1
Ноорузбек <nooruz@outlook.com>
2022-02-14 22:19:27 +0600

    14.02.2022

--- RetailTrade.DialogService/Dialog.cs
+++ RetailTrade.DialogService/Dialog.cs
@@ -1,10 +1,28 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RetailTrade.DialogService
 {
     public class Dialog : Control
     {
+        #region Dependency
+
+        public static readonly DependencyProperty FontAwesomeProperty =
+            DependencyProperty.Register(nameof(FontAwesome), typeof(FontFamily), typeof(Dialog), new PropertyMetadata(new FontFamily()));
+
+        #endregion        
+
+        #region Public Properties
+
+        public FontFamily FontAwesome
+        {
+            get => (FontFamily)GetValue(FontAwesomeProperty);
+            set => SetValue(FontAwesomeProperty, value);
+        }
+
+        #endregion
+
         static Dialog()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Dialog), new FrameworkPropertyMetadata(typeof(Dialog)));
