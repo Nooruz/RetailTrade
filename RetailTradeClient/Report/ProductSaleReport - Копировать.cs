90084e44bcfdb9bc1f7ccd15d5e3471321208363
Ноорузбек <nooruz@outlook.com>
2022-03-20 21:49:18 +0600

    20.03.2022

---
+++ RetailTradeClient/Report/ProductSaleReport - Копировать.cs
@@ -0,0 +1,53 @@
+using RetailTrade.Domain.Models;
+using RetailTradeClient.State.Users;
+using System;
+using System.Collections.Generic;
+
+namespace RetailTradeClient.Report
+{
+    public partial class ProductSaleCashlessReport
+    {
+        #region Private Members
+
+        private readonly IUserStore _userStore;
+
+        #endregion
+
+        #region Constructor
+
+        public ProductSaleReport(IUserStore userStore)
+        {
+            InitializeComponent();
+            _userStore = userStore;
+        }
+
+        #endregion
+
+        #region Public Voids
+
+        public void SetValues(Receipt receipt, IEnumerable<Sale> sales)
+        {
+            try
+            {
+                DataSource = sales;
+                CashDate.Text = $"Дата: {receipt.DateOfPurchase.ToShortDateString()}";
+                CashTime.Text = $"Время: {receipt.DateOfPurchase.ToShortTimeString()}";
+                CashReceipt.Text = $"Товарный чек № {receipt.Id:D6}";
+                lbUserFullName.Text = _userStore.CurrentUser.FullName;
+                lbDeposited.Text = $"{receipt.Deposited:N2} сом";
+                lbChange.Text = $"{receipt.Change:N2} сом";
+                if (_userStore.Organization != null)
+                {
+                    OrganizationName.Text = _userStore.Organization.Name;
+                    Address.Text = _userStore.Organization.Address;
+                }
+            }
+            catch (Exception)
+            {
+                //ignore
+            }
+        }
+
+        #endregion
+    }
+}
