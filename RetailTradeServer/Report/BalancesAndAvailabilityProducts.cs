ff1215a72ce39c19b09eda66a8aa1874176bd44c
Ноорузбек <nooruz@outlook.com>
2022-05-20 08:59:54 +0600

    Balances and availability of goods

---
+++ RetailTradeServer/Report/BalancesAndAvailabilityProducts.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace RetailTradeServer.Report
+{
+    public partial class BalancesAndAvailabilityProducts
+    {
+        public BalancesAndAvailabilityProducts()
+        {
+            InitializeComponent();
+        }
+    }
+}
