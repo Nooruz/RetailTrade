851d3c0ffd7b9ee1e94fd673accc7f711b7a9611
Ноорузбек <nooruz@outlook.com>
2022-09-01 17:01:30 +0600

    01.09.2022

---
+++ RetailTrade.Domain/Models/Document.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace RetailTrade.Domain.Models
+{
+    public class Document : DomainObject
+    {
+        #region Private Members
+
+        private int _documentTypeId;
+        private DateTime _createdDate;
+        private int _userId;
+        private decimal _amount;
+        private int _wareHouseId;
+        private string _comment;
+
+        #endregion
+
+        #region Public Properties
+
+        public int DocumentTypeId
+        {
+            get => _documentTypeId;
+            set
+            {
+                _documentTypeId = value;
+                OnPropertyChanged(nameof(DocumentTypeId));
+            }
+        }
+        public DateTime CreatedDate
+        {
+            get => _createdDate;
+            set
+            {
+                _createdDate = value;
+                OnPropertyChanged(nameof(CreatedDate));
+            }
+        }
+        public int UserId
+        {
+            get => _userId;
+            set
+            {
+                _userId = value;
+                OnPropertyChanged(nameof(UserId));
+            }
+        }
+        public decimal Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged(nameof(Amount));
+            }
+        }
+        public int WareHouseId
+        {
+            get => _wareHouseId;
+            set
+            {
+                _wareHouseId = value;
+                OnPropertyChanged(nameof(WareHouseId));
+            }
+        }
+        public string Comment
+        {
+            get => _comment;
+            set
+            {
+                _comment = value;
+                OnPropertyChanged(nameof(Comment));
+            }
+        }
+        public DocumentType DocumentType { get; set; }
+        public User User { get; set; }
+        public WareHouse WareHouse { get; set; }
+
+        #endregion
+    }
+}
---
+++ RetailTrade.Domain/Models/DocumentType.cs
@@ -0,0 +1,16 @@
+﻿namespace RetailTrade.Domain.Models
+{
+    public class DocumentType : DomainObject
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+    }
+}
